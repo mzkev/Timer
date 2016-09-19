6c9d84e1c782d86c4e16d8ced4058709a3c070cd
thebrianemory <the@brianemory.com>
2016-09-18 20:18:46 -0700

    Add TimerForm component

--- public/app.js
+++ public/app.js
@@ -35,3 +35,33 @@ const EditableTimerList = React.createClass({
     );
   },
 });
+
+const TimerForm = React.createClass({
+  render: function () {
+    const submitText = this.props.title ? 'Update' : 'Create';
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='ui form'>
+            <div className='field'>
+              <label>Title</label>
+              <input type='text' defaultValue={this.props.title} />
+            </div>
+            <div className='field'>
+              <label>Project</label>
+              <input type='text' defaultValue={this.props.project} />
+            </div>
+            <div className='ui two bottom attached buttons'>
+              <button className='ui basic blue button'>
+                {submitText}
+              </button>
+              <button className='ui basic red button'>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  },
+});
