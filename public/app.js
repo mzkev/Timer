d8af35fa9a39314d58eb24af652fedac4b2f38de
thebrianemory <the@brianemory.com>
2016-09-18 19:56:34 -0700

    Add EditableTimerList component

--- public/app.js
+++ public/app.js
@@ -12,3 +12,26 @@ const TimersDashboard = React.createClass({
     );
   },
 });
+
+const EditableTimerList = React.createClass({
+  render: function () {
+    return (
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Learn extreme ironing'
+          project='World Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  },
+});
