b406b86fe7d298487b1864c2436800273164bd47
thebrianemory <the@brianemory.com>
2016-09-18 20:43:58 -0700

    Add Timer compenent

--- public/app.js
+++ public/app.js
@@ -83,3 +83,37 @@ const ToggleableTimerForm = React.createClass ({
     }
   },
 });
+
+const Timer = React.createClass({
+  render: function () {
+    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='header'>
+            {this.props.title}
+          </div>
+          <div className='meta'>
+            {this.props.project}
+          </div>
+          <div className='center aligned description'>
+            <h2>
+              {elapsedString}
+            </h2>
+          </div>
+          <div className='extra content'>
+            <span className='right floated edit icon'>
+              <i className='edit icon'></i>
+            </span>
+            <span className='right floated trash icon'>
+              <i className='trash icon'></i>
+            </span>
+          </div>
+        </div>
+        <div className='ui bottom attached blue basic button'>
+          Start
+        </div>
+      </div>
+    );
+  },
+});
