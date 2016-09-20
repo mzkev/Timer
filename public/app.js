af1f381dc97a53dbd5e7f246971a52e89debf0c9
thebrianemory <the@brianemory.com>
2016-09-19 22:08:20 -0700

    Add state to EditableTimer

--- public/app.js
+++ public/app.js
@@ -55,10 +55,16 @@ const EditableTimerList = React.createClass({
 });
 
 const EditableTimer = React.createClass({
+  getInitialState: function () {
+    return {
+      editFormOpen: false,
+    };
+  },
   render: function () {
-    if (this.props.editFormOpen) {
+    if (this.state.editFormOpen) {
       return (
         <TimerForm
+          id={this.props.id}
           title={this.props.title}
           project={this.props.project}
           />
@@ -66,6 +72,7 @@ const EditableTimer = React.createClass({
     } else {
       return (
         <Timer
+          id={this.props.id}
           title={this.props.title}
           project={this.props.project}
           elapsed={this.props.elapsed}
