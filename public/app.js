a12adfb310e67dce177e4143e119240af901de54
thebrianemory <the@brianemory.com>
2016-09-19 22:00:01 -0700

    Update EditableTimerList to receive props

--- public/app.js
+++ public/app.js
@@ -35,22 +35,20 @@ const TimersDashboard = React.createClass({
 
 const EditableTimerList = React.createClass({
   render: function () {
+    const timers = this.props.timers.map((timers) =>
+  (
+    <EditableTimer
+      key={timer.id}
+      id={timer.id}
+      title={timer.title}
+      project={timer.project}
+      elapsed={timer.elapsed}
+      runningSince={timer.runningSince}
+    />
+  ));
     return (
       <div id='timers'>
-        <EditableTimer
-          title='Learn React'
-          project='Web Domination'
-          elapsed='8986300'
-          runningSince={null}
-          editFormOpen={false}
-        />
-        <EditableTimer
-          title='Learn extreme ironing'
-          project='World Domination'
-          elapsed='3890985'
-          runningSince={null}
-          editFormOpen={true}
-        />
+        {timers}
       </div>
     );
   },
