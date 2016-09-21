dd6a6aa052b3c90db4c8c8d576e33cb3463f6367
thebrianemory <the@brianemory.com>
2016-09-20 21:22:17 -0700

    Add event handlers to TimerForm

--- public/app.js
+++ public/app.js
@@ -84,25 +84,42 @@ const EditableTimer = React.createClass({
 });
 
 const TimerForm = React.createClass({
+  handleSubmit: function () {
+    this.props.onFormSubmit()({
+      id: this.props.id,
+      title: this.refs.title.value,
+      project: this.refs.project.value,
+    });
+  },
   render: function () {
-    const submitText = this.props.title ? 'Update' : 'Create';
+    const submitText = this.props.id ? 'Update' : 'Create';
     return (
       <div className='ui centered card'>
         <div className='content'>
           <div className='ui form'>
             <div className='field'>
               <label>Title</label>
-              <input type='text' defaultValue={this.props.title} />
+              <input type='text' ref='title'
+                defaultValue={this.props.title}
+              />
             </div>
             <div className='field'>
               <label>Project</label>
-              <input type='text' defaultValue={this.props.project} />
+              <input type='text' ref='project'
+                defaultValue={this.props.project}
+              />
             </div>
             <div className='ui two bottom attached buttons'>
-              <button className='ui basic blue button'>
+              <button
+                className='ui basic blue button'
+                onClick={this.handleSubmit}
+              >
                 {submitText}
               </button>
-              <button className='ui basic red button'>
+              <button
+                className='ui basic red button'
+                onClick={this.props.onFormClose}
+              >
                 Cancel
               </button>
             </div>
