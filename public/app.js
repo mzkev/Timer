9870756c47fe72f467129f93f3a700311cf9c5c2
thebrianemory <the@brianemory.com>
2016-09-21 19:05:20 -0700

    Add forceUpdate() to Timer component

--- public/app.js
+++ public/app.js
@@ -230,11 +230,17 @@ const ToggleableTimerForm = React.createClass ({
 });
 
 const Timer = React.createClass({
+  componentDidMount: function () {
+    this.forceUpdateInterval = setInterval(() => this.forceUpdate(), 50);
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.forceUpdateInterval);
+  },
   handleTrashClick: function () {
     this.props.onTrashClick(this.props.id);
   },
   render: function () {
-    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+    const elapsedString = helpers.renderElapsedString(this.props.elapsed, this.props.runningSince);
     return (
       <div className='ui centered card'>
         <div className='content'>
